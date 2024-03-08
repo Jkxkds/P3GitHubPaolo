953ae4263362aa59d5c5cf48f962c11165891a89
Jkxkds <108697221+Jkxkds@users.noreply.github.com>
2024-03-08 09:01:36 +0100

    realizando el commit de mi programa sencillo

---
+++ Aplicacion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+
+public class ReplaceScanner {
+    public static void main(String[]args){
+   Scanner entrada=new Scanner(System.in);
+        System.out.println("Introduce una frase");
+        String cadena=entrada.nextLine(); 
+        System.out.println(cadena); 
+    }
+}
